8eb699f1ae270fd3863d07d2b05fdceb59fe4485
Adam Sinck <absinck+github@grueslayer.com>
2016-09-05 16:39:52 -0700

    Make the program work with a temperature sensor

--- parse.py
+++ parse.py
@@ -17,14 +17,13 @@
 
 
 class FanController:
-
     import time #if only it was this easy in real life...
-    
     import RPi.GPIO as GPIO #the class for the pi
-
+    import Adafruit_DHT #the class for the temp sensor
+    
     def __init__(self):
         self.GPIO.setmode(self.GPIO.BOARD) #set up the board
-
+        self.sensor = self.Adafruit_DHT.DHT11
         #try to open the config file
         configFile = None
         try:
@@ -35,12 +34,12 @@ class FanController:
 
         #this is a dictionary of values
         self.variables = {
-            "tempSensor":4,
-            "fan1":17,
-            "fan2":27,
+            "tempSensor":23,
+            "fan1":11,
+            "fan2":13,
             "lowTemp":75,
             "highTemp":85,
-            "checkDelay":1, #seconds
+            "checkDelay":60, #seconds
             "reverseLogic":0
         }
 
@@ -67,11 +66,18 @@ class FanController:
                         self.variables[varName] = int(value)
         print("closing config file...")
         configFile.close()
+        
+        try:
+            self.GPIO.setup(self.variables["tempSensor"], self.GPIO.IN)
+        except:
+            print("Error: Could not initialize temperature sensor on pin %d." %self.variables["tempSensor"])
+            exit(1)
+
         self.initFans()
 
         self.fansOn = False;
 
-
+        
         #do some output to make sure that the values were picked up
         for i in self.variables:
             print(i, self.variables[i])
@@ -79,8 +85,17 @@ class FanController:
 
     #this gets the value of the sensor
     def getTemp(self):
-        import random as r
-        return r.randint(self.variables["lowTemp"] - 10, self.variables["highTemp"] + 10) #...yeah, this really should be an actual reading
+        # while True:
+        #     print(self.GPIO.input(self.variables["tempSensor"]))
+        print("Obtaining temp...")
+        humidity, temperature = self.Adafruit_DHT.read_retry(self.sensor, self.variables["tempSensor"])
+        print("Finished obtaining temperature.")
+        temperature = (temperature * 9.0/5.0) + 32
+        if humidity != None and temperature != None:
+            print('Temp={0:0.1f}F, Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print("Reading failed.")
+        return temperature
 
     #this returns true or false, based on if the fans are on or off
     def checkFans():
@@ -116,12 +131,12 @@ class FanController:
         try:
             self.GPIO.setup(self.variables["fan1"], self.GPIO.OUT)
         except:
-            print("Fan on pin %d failed to initialize." %self.variables["fan1"])
+            print("Error: Fan on pin %d failed to initialize." %self.variables["fan1"])
             exit(1)
         try:
             self.GPIO.setup(self.variables["fan2"], self.GPIO.OUT)
         except:
-            print("Fan on pin %d failed to initialize." %self.variables["fan2"])
+            print("Error: Fan on pin %d failed to initialize." %self.variables["fan2"])
             exit(1)
         self.turnFansOff()
 
@@ -150,7 +165,8 @@ class FanController:
                         self.turnFansOff()
                 else:
                     print("Temperature within range.")
-                
+
+                print("\n")
                 self.time.sleep(self.variables["checkDelay"])
             except KeyboardInterrupt:
                 print("\n\nProgram Exiting...")
