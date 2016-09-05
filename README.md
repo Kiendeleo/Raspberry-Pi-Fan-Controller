463a14848764b187dd888d3a033cc9ab05507729
Adam Sinck <absinck+github@grueslayer.com>
2016-09-05 10:41:15 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Raspberry Pi Fan Controller
---
+++ parse.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+
+###########################################################################
+##
+## This class controls fans. It can use a config file, or use default
+## values. It expects a temperature sensor. It expects two fans, and uses a
+## low value and a high value for deciding if it should toggle the fans or
+## not. 
+##
+## This program uses GPIO 11 and 13 for the fans, and 4 for the temp sensor.
+##
+## Any variable in the variable list can be changed through the config file.
+##
+## Note: This assumes active low.
+##
+###########################################################################
+
+
+class FanController:
+
+    import time #if only it was this easy in real life...
+    
+    import RPi.GPIO as GPIO #the class for the pi
+
+    def __init__(self):
+        self.GPIO.setmode(self.GPIO.BOARD) #set up the board
+
+        #try to open the config file
+        configFile = None
+        try:
+            configFile = open("config.cfg")
+        except:
+            print("Error: Config file config.cfg not found.\n")
+            exit(1)
+
+        #this is a dictionary of values
+        self.variables = {
+            "tempSensor":4,
+            "fan1":17,
+            "fan2":27,
+            "lowTemp":75,
+            "highTemp":85,
+            "checkDelay":1, #seconds
+            "reverseLogic":0
+        }
+
+        print("opening config file...")
+        #go through the config file, reading values
+        for line in configFile:
+            myLine = line.strip() #strip the whitespace off the line
+        
+            #make sure there's a line and it's not a comment
+            if (len(myLine) > 0 and myLine[0] != "#"):
+                #if there are any inline comments, take them off
+                myLine = myLine.split(";")[0].strip()
+                #split the string over the '='
+                pair = myLine.split("=") 
+                #print(pair)
+                #make sure there are two values (there was an =); otherwise,
+                #ignore the line
+                if (len(pair) == 2):
+                    #set the var name and the value, stripping the semicolon
+                    varName, value = pair[0], pair[1]
+                    #print(varName, value)
+                    #if the variable exists, set it
+                    if varName in self.variables:
+                        self.variables[varName] = int(value)
+        print("closing config file...")
+        configFile.close()
+        self.initFans()
+
+        self.fansOn = False;
+
+
+        #do some output to make sure that the values were picked up
+        for i in self.variables:
+            print(i, self.variables[i])
+    
+
+    #this gets the value of the sensor
+    def getTemp(self):
+        import random as r
+        return r.randint(self.variables["lowTemp"] - 10, self.variables["highTemp"] + 10) #...yeah, this really should be an actual reading
+
+    #this returns true or false, based on if the fans are on or off
+    def checkFans():
+        return self.fansOn
+    
+    #This turns the fans off.
+    def turnFansOn(self):
+        if (self.variables["reverseLogic"] == 0):
+            self.GPIO.output(self.variables["fan1"], self.GPIO.HIGH)
+            self.GPIO.output(self.variables["fan2"], self.GPIO.HIGH)
+            self.fansOn = True
+        else:# self.variables["reverseLogic"] == 1:
+            self.GPIO.output(self.variables["fan1"], self.GPIO.LOW)
+            self.GPIO.output(self.variables["fan2"], self.GPIO.LOW)
+            self.fansOn = True
+        print("Fans turned on.")
+
+
+    #This turns the fans on.
+    def turnFansOff(self):
+        if (self.variables["reverseLogic"] == 0):
+            self.GPIO.output(self.variables["fan1"], self.GPIO.LOW)
+            self.GPIO.output(self.variables["fan2"], self.GPIO.LOW)
+            self.fansOn = False
+        else:# self.variables["reverseLogic"] == 1
+            self.GPIO.output(self.variables["fan1"], self.GPIO.HIGH)
+            self.GPIO.output(self.variables["fan2"], self.GPIO.HIGH)
+            self.fansOn = False
+        print("Fans turned off.")
+        
+    def initFans(self):
+        print("Initializing fans...")
+        try:
+            self.GPIO.setup(self.variables["fan1"], self.GPIO.OUT)
+        except:
+            print("Fan on pin %d failed to initialize." %self.variables["fan1"])
+            exit(1)
+        try:
+            self.GPIO.setup(self.variables["fan2"], self.GPIO.OUT)
+        except:
+            print("Fan on pin %d failed to initialize." %self.variables["fan2"])
+            exit(1)
+        self.turnFansOff()
+
+        # if (self.variables["reverseLogic"] == 0):
+        #     self.GPIO.setup(self.variables["fan1"], self.GPIO.OUT, inital=self.GPIO.HIGH)
+        #     self.GPIO.setup(self.variables["fan2"], self.GPIO.OUT, inital=self.GPIO.HIGH)
+        # if (self.variables["reverseLogic"] == 1):
+        #     self.GPIO.setup(self.variables["fan1"], self.GPIO.OUT, inital=self.GPIO.LOW)
+        #     self.GPIO.setup(self.variables["fan2"], self.GPIO.OUT, inital=self.GPIO.LOW)
+
+    
+    #This starts the monitoring
+    def start(self):
+        while(1):
+            try:
+                #This checks to see if the temperature is outside the allowed
+                #range. If it is, it toggles the fans accordingly.
+                currentTemp = self.getTemp()
+                if (currentTemp >= self.variables["highTemp"]):
+                    print("Temperature above limit.")
+                    if (not self.fansOn):
+                        self.turnFansOn()
+                elif (currentTemp <= self.variables["lowTemp"]):
+                    print("Temperature below limit.")
+                    if (self.fansOn):
+                        self.turnFansOff()
+                else:
+                    print("Temperature within range.")
+                
+                self.time.sleep(self.variables["checkDelay"])
+            except KeyboardInterrupt:
+                print("\n\nProgram Exiting...")
+                self.GPIO.cleanup()
+                print("Goodbye!\n")
+                break
+
+#The main function of this program. Mainly used for testing.
+def main():
+    print("\n\nWARNING: This program does not to actual temperature readings.\n\n")
+    f = FanController()
+    f.start()
+
+
+
+#This starts the program
+if __name__ == "__main__":
+    main()
